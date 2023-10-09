2a83e127ff441ee039d5fe4a1764579242928394
Nicholas Breno <50603874+nkbreno33@users.noreply.github.com>
2023-10-09 15:05:49 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const acertou = document.querySelector('.container__texto h1');
+const iniciarBt = document.querySelector('.btnComecar');
+
+let numeroMaximo = 10;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+let chute;
+let tentativas = 1;
+
+iniciarBt.addEventListener('click', function () {
+    while (chute != numeroSecreto) {
+        chute = prompt(`Digite um número entre 1 e ${numeroMaximo}`);
+          
+        if (chute == numeroSecreto && tentativas > 1) {
+            acertou.innerHTML = `Você acertou com ${tentativas} tentativas`
+        }
+        else if (chute == numeroSecreto && tentativas <= 1){
+            acertou.innerHTML = `Você acertou com ${tentativas} tentativa`
+        }
+        else {
+            if (chute > numeroSecreto){
+                alert(`O numero secreto é menor que ${chute}`);
+            }   else {
+                alert(`o numero secreto é maior que ${chute}`);
+            }
+            tentativas ++; 
+        }
+    } 
+})
